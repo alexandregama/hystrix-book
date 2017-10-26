0acc406cd9c9ee5b2adebfdaf50cbcf1b584e01b
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-10-26 07:59:38 -0200

    Adds a few examples using Hystrix synchronously and asynchronously

---
+++ src/test/java/com/hystrix/documentation/part1/CommandHelloWorldWithTimeoutWithoutFallback.java
@@ -0,0 +1,23 @@
+package com.hystrix.documentation.part1;
+
+import com.hystrix.book.chapter1.Pause;
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+public class CommandHelloWorldWithTimeoutWithoutFallback extends HystrixCommand<String> {
+
+	private String name;
+
+	public CommandHelloWorldWithTimeoutWithoutFallback(String name) {
+		super(HystrixCommandGroupKey.Factory.asKey("CommandHelloWorldWithTimeout"));
+		this.name = name;
+	}
+
+	@Override
+	protected String run() throws Exception {
+		Pause.waitFor(5);
+
+		return "Hello " + name;
+	}
+
+}
---
+++ src/test/java/com/hystrix/documentation/part1/CommandHelloWorldWithoutFallback.java
@@ -0,0 +1,20 @@
+package com.hystrix.documentation.part1;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+public class CommandHelloWorldWithoutFallback extends HystrixCommand<String> {
+
+	private String name;
+
+	protected CommandHelloWorldWithoutFallback(String name) {
+		super(HystrixCommandGroupKey.Factory.asKey("CommandHelloWorld"));
+		this.name = name;
+	}
+
+	@Override
+	protected String run() throws Exception {
+		return "Hello " + name;
+	}
+
+}
